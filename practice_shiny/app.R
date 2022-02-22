f18ad423c7698c2df286ea618592209c54f969f8
connerjainese <connerjainese@gmail.com>
2022-02-21 17:40:23 -0800

    add slider widget to tab 1

--- practice_shiny/app.R
+++ practice_shiny/app.R
@@ -9,28 +9,31 @@
 # attach necessary packages
 library(shiny)
 library(tidyverse)
+library(palmerpenguins)
+
+penguins <- penguins
 
 # Create the user interface:
 ui <- navbarPage("Tick, Tick, Boom: Tick population (Family Acari) distributions in California",
-                 tabPanel("Tick Thing 1",
+                 tabPanel("Human Lyme Disease",
                           sidebarLayout(
-                            sidebarPanel("Widget 1 Here"),
+                            sidebarPanel("Widget 1 Here",
+                                         sliderInput(inputId = "year",
+                                                     label = "Select Years",
+                                                     min = 2007,
+                                                     max = 2009,
+                                                     value = c(2007, 2009))),
                             mainPanel("Graph/Map 1 Here")
                           )),
-                 tabPanel("Tick Thing 2",
+                 tabPanel("Life Stage Map",
                           sidebarLayout(
-                            sidebarPanel("Widget 2 Here"),
+                            sidebarPanel("Widget 2A Here"),
                             mainPanel("Graph/Map 2 Here")
                           )),
-                 tabPanel("Tick Thing 3",
+                 tabPanel("Tejon Ticks",
                           sidebarLayout(
                             sidebarPanel("Widget 3 Here"),
                             mainPanel("Graph/Map 3 Here")
-                          )),
-                 tabPanel("Tick Thing 4",
-                          sidebarLayout(
-                            sidebarPanel("Widget 4 Here"),
-                            mainPanel("Graph/Map 4 Here")
                           ))
 )
 
