400e54d258a0705bf0076f453ef11b4c11ea933d
kcring <kacie_ring@ucsb.edu>
2022-02-22 12:44:45 -0800

    Lyme incidence data, cannot get county to be interactive

--- practice_shiny/app.R
+++ practice_shiny/app.R
@@ -11,8 +11,19 @@ library(shiny)
 library(tidyverse)
 library(palmerpenguins)
 library(bslib)
+library(plotly)
+#install.packages("ggthemes")
+library(ggthemes)
 
 #penguins <- penguins
+
+long <- read_csv("long.csv")
+tick_graph <-
+  ggplot(data = long, aes(x = year, y = incidence, group = County)) +
+  geom_line(aes(color = County)) +
+  labs(x = "Year", y = "Lyme Disease Incidence", title = "Human Lyme Disease Incidence 2011-2020")
+theme_minimal()
+
 
 # Create the user interface:
 # using navbarPage() to setup tabs
@@ -25,21 +36,14 @@ ui <- navbarPage(theme = bs_theme(bootswatch = "flatly"),
                             # create sidebar panel that will house widgets
                             sidebarPanel("Human Lyme Disease Occurence by Year and County",
                                          # add slider input
-                                         sliderInput(inputId = "year",
-                                                     label = "Select Years",
-                                                     min = 2007,
-                                                     max = 2009,
-                                                     value = c(2007, 2009),
-                                                     timeFormat = TRUE),
                                          # add checkbox group
-                                         checkboxGroupInput(inputId = "island",
-                                                            label = "Select Counties",
-                                                            choices = c("Torgersen",
-                                                                        "Biscoe",
-                                                                        "Dream"),
-                                                            selected = "Torgersen")),
+                                         checkboxGroupInput(inputId = "County",
+                                                            label = "Select County",
+                                                            choices = c("Sierra" ,"Sacramento","Santa Barbara" , "Calaveras" ,"Ventura", "Los Angeles","Sonoma", "San Francisco","Marin" ,"Mariposa","Lassen","Napa","Kings","San Diego","Placer", "San Francisco", "Marin","Mariposa",  "Lassen", "Napa", "Shasta","Monterey", "Trinity","Mendocino","Inyo","Mono","Tuolumne","Solano", "San Bernardino", "Contra Costa" ,"Alpine","El Dorado","Yolo", "Yuba", "San Benito", "Humboldt", "Riverside","Kern","Colusa" ,"Del Norte" ,"Modoc", "Fresno", "Madera", "Santa Clara", "Tehama" ,"San Joaquin" ,"Alameda","Nevada","Butte", "Merced", "Tulare" , "Stanislaus","Orange","Imperial","Sutter", "Amador", "Lake" ,"Plumas" ,"San Mateo", "Siskiyou", "Santa Cruz", "Glenn", "San Luis Obispo"
+                                                            ))),
                             # create main panel for output
-                            mainPanel("Graph/Map 1 Here")
+                            mainPanel("Graph/Map 1 Here",
+                                      plotlyOutput(outputId = "lyme_plot"))
                           )),
                  # second tab
                  tabPanel("Life Stage Map",
@@ -91,25 +95,38 @@ ui <- navbarPage(theme = bs_theme(bootswatch = "flatly"),
                                                             selected = NULL)
 ),
                             # create main panel for output
-                            mainPanel("Graph/Map 3 Here",
-                                      plotOutput(outputId = tejon_tick))
+                            mainPanel("Graph/Map 3 Here")
+                                      #plotOutput(outputId = tejon_tick))
                           ))
 )
 
 # Create the server function:
 server <- function(input, output) {
+  ##Pt.1 Lyme incidence
+  lyme_incidence <- reactive({
+    long %>%
+      #dplyr::group_by(County)
+      dplyr::filter(County %in% input$County)
+  })
 
-  ## Pt 3: Tick Seasonality - I'm not getting this to display and I don't know why
-  tejon_tick_app_2 <- reactive({
-    tejon_tick_app_2 %>%
-      filter(month == input$month) %>%
-      filter(plot == input$plot)
+  output$lyme_plot <- renderPlotly({
+    ggplot(data = long, aes(x = year, y = incidence, group = County)) +
+      geom_line(aes(color = County)) +
+      labs(x = "Year", y = "Lyme Disease Incidence", title = "Human Lyme Disease Incidence 2011-2020") +
+    theme_minimal()
+
+
+#Pt 3: Tick Seasonality - I'm not getting this to display and I don't know why
+ tejon_tick_app_2 <- reactive({
+   tejon_tick_app_2 %>%
+     filter(month == input$month) %>%
+     filter(plot == input$plot)
   }) #end tejon reactive
 
-  output$tejon_tick <- renderPlot({
+ output$tejon_tick <- renderPlot({
     ggplot(data = tejon_tick_app_2(), aes(x = month, y = n))+
       geom_bar(stat = "identity")+
-      theme_bw()
+     theme_bw()
   })
 
 }
